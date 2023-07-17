52262873fd9f682e7d4a35664e210afd17fb25d1
Sudjai Jannual <toem1234@gmail.com>
2023-07-18 04:19:20 +0700

    Initial Project

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css"
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
+
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}  
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
